42f608bd441ea88becab065e1b558f738c745e7c
Dimash <dimash20080808gmail.com>
2026-05-02 11:45:55 +0500

    TSIS

---
+++ TSIS4/config.py
@@ -0,0 +1,8 @@
+# PostgreSQL connection settings
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "snake_db",
+    "user": "postgres",
+    "password": "happymylife56_2",
+    "port": 5432
+}
